c164ceaab1e2d832b383355aab7153331827d89d
RyuRyu-0817 <rusuzumi@outlook.jp>
2023-12-31 02:57:39 +0900

    ハンバーガーメニュー実装

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mdeditor/', include('mdeditor.urls')),
     path('auth/', include('pevsearch.authentication.urls')),
     path('api/', include("pevsearch.urls")),
 
